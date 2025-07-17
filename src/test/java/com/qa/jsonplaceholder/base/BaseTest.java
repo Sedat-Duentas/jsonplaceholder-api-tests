c92d6c6d649285acfe2a84ac7c87d12025afa291
Sedat Düntas <sduentas@nelta.de>
2025-07-17 15:02:58 +0200

    gitlab finish

--- src/test/java/com/qa/jsonplaceholder/base/BaseTest.java
+++ src/test/java/com/qa/jsonplaceholder/base/BaseTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 
 public abstract class BaseTest {
 
-    // Diese Methode wird einmalig vor allen Tests in allen erbenden Klassen ausgeführt.
+    // Diese Methode wird einmalig vor allen Tests in allen erbenden Klassen ausgeführt
     @BeforeAll
     static void setup() {
         // Die Konfiguration der Basis-URI und des Loggings direkt hier
